413213683eb145a28fc9eb41018380da2a7f70fb
th1enq <thienchy3305@gmail.com>
2026-05-06 13:49:09 +0700

    fix: fix runtime build env

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -6,7 +6,7 @@ window.__APP_CONFIG__ = window.__APP_CONFIG__ ?? {
   oauth: {
     googleClientId: '64832597305-4ck1svh88e5ptudtadtgco8s0664s9s6.apps.googleusercontent.com',
     facebookAppId: '',
-    redirectBase: 'http://159.223.38.173:3000/auth/google/callback',
+    redirectBase: 'http://159.223.38.173.nip.io:3000/auth/callback/google',
   },
 }
 
